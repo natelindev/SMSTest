fd7ace5fb4e80a807012e58b0e2cc9304a75e215
Nathaniel <linlilulll@gmail.com>
2015-08-26 11:14:14 +0800

    Add the function of the sending SMS.

--- app/src/main/java/com/example/lll/smstest/MainActivity.java
+++ app/src/main/java/com/example/lll/smstest/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.lll.smstest;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -18,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView content;
     private IntentFilter receiverFilter;
     private MessageReceiver messageReceiver;
+    private EditText to;
+    private EditText msgInput;
+    private Button send;
+    private IntentFilter sendFilter;
+    private SendStatusReceiver sendStatusReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +41,31 @@ public class MainActivity extends AppCompatActivity {
         receiverFilter.setPriority(100);
         messageReceiver = new MessageReceiver();
         registerReceiver(messageReceiver,receiverFilter);
+        to = (EditText) findViewById(R.id.to);
+        msgInput = (EditText) findViewById(R.id.msg_input);
+        send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SmsManager smsManager = SmsManager.getDefault();
+                Intent sentIntent = new Intent("SENT_SMS_ACTION");
+                PendingIntent  pi = PendingIntent.getBroadcast(
+                        MainActivity.this,0,sentIntent,0);
+                smsManager.sendTextMessage(to.getText().toString(),null,
+                        msgInput.getText().toString(),null,null);
+            }
+        });
+        sendFilter = new IntentFilter();
+        sendFilter.addAction("SENT_SMS_ACTION");
+        sendStatusReceiver = new SendStatusReceiver();
+        registerReceiver(sendStatusReceiver,sendFilter);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         unregisterReceiver(messageReceiver);
+        unregisterReceiver(sendStatusReceiver);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,6 +108,19 @@ public class MainActivity extends AppCompatActivity {
                 abortBroadcast();
             }
 
+        }
+    }
+
+    class SendStatusReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context,Intent intent){
+            if (getResultCode() == RESULT_OK) {
+                Toast.makeText(context,"Message Sent",Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(context,"Send failed",Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
